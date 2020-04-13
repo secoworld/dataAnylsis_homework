2e7598010d10445da45acc0716760ee17bb37a25
longtao <chinaliulongtao@163.com>
2020-04-13 21:46:41 +0800

    "更改获取json的方法，去除cookie"

--- get_homework.py
+++ get_homework.py
@@ -3,14 +3,20 @@ import os
 import json
 
 # 设置请求头部信息
-header = {
-    'cookie': "UM_distinctid=1707a831af22d0-09b52444af64bf-6701b35-100200-1707a831af3111; _log_user_id=67ef3219e9957a4aee072714715f8afa; sharesessionid=f8bdf594f9c4e0eab3b634f32704743f; login_type=WX; csrftoken=hm6AJZrTZP70b8ROqT8K7RvO6onwnwMR; sessionid=fz2n3to0zbqn7szpkg5it93vh5gu7dza; k=46887; django_language=zh",
-    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-    'referer': "https://next.xuetangx.com/learn/tjnu08091002372/tjnu08091002372/1516372/exercise/1392904",
-    'x-csrftoken': "QLdw3KKDQOLPWIVVvOsJyXKI2wq97GZr",
-    'accept-encoding': "gzip, deflate, br",
-    'accept-language': 'zh-CN,zh;q=0.9'
+path = "./header.json"      # 用于存储requests的header， json 中的格式如下所示
+"""
+{
+    "cookie": " 你的cookie ",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+    "referer": "https://next.xuetangx.com/learn/tjnu08091002372/tjnu08091002372/1516372/exercise/1392904",
+    "x-csrftoken": "QLdw3KKDQOLPWIVVvOsJyXKI2wq97GZr",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-CN,zh;q=0.9"
 }
+"""
+with open(path, 'r') as f:
+    header = json.load(f)
+
 
 url = 'https://next.xuetangx.com/api/v1/lms/exercise/get_exercise_list/'
 
